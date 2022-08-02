6ce14ffc15b0e993573160099b2b155f4dd31338
Maya Lorimer <maya.lorimer@yahoo.com>
2022-08-02 14:20:30 -0700

    wineSeeds and seeds.js updated

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,1 +1,17 @@
 //seeds.js
+const db = require('../config/connection');
+const { Wine } = require('../models');
+const wineSeeds = require('./wineSeeds.json');
+//const userSeeds = require('./userSeeds.json');
+
+db.once('open', async () => {
+  try {
+    await Wine.deleteMany({});
+    await Wine.create(wineSeeds);
+
+    console.log('all done!');
+    process.exit(0);
+  } catch (err) {
+    throw err;
+  }
+});
