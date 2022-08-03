66b45657db299e417b3958bc0eaefca1282f2f54
xndcmd <xndcmd@gmail.com>
2022-08-03 11:05:16 -0700

    React develop server now deploys successfully (with bugs)

--- client/src/App.js
+++ client/src/App.js
@@ -65,23 +65,12 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        {/* <>
-          <Navbar /> */}
           <Routes>
-            {/* <Route 
-              path="/" 
-              element={<SearchBooks/>} 
-            />
-            <Route 
-              path="/saved" 
-              element={<SavedBooks/>} 
-            /> */}
             <Route 
               path='*' 
               element={<h1 className="display-2">Wrong page!</h1>}
             />
           </Routes>
-        {/* </> */}
       </Router>
     </ApolloProvider>
   );
