9cbe9d581c352cabb9b2093936e40d54e09b103f
xndcmd <xndcmd@gmail.com>
2022-08-03 10:54:20 -0700

    adjusting Home.js code to reflect Wine.js model

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,37 +15,13 @@ import {
 
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations';
-import { removeBookId } from '../utils/localStorage';
 
 import Auth from '../utils/auth';
 
-const SavedBooks = () => {
+const AllWine = () => {
   const { loading, data } = useQuery(QUERY_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
   const userData = data?.me || {};
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = async (bookId) => {
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
-    try {
-      const { data } = await removeBook({
-        variables: { bookId },
-      });
-
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
-    } catch (err) {
-      console.error(err);
-    }
-  };
 
   if (loading) {
     return <h2>LOADING...</h2>;
@@ -55,37 +31,37 @@ const SavedBooks = () => {
     <>
       <Jumbotron fluid className="text-light bg-dark">
         <Container>
-          <h1>Viewing {userData.username}'s books!</h1>
+          <h1>Viewing {userData.username}'s wines!</h1>
         </Container>
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks?.length
-            ? `Viewing ${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? 'book' : 'books'
+          {userData.AllWine?.length
+            ? `Viewing ${userData.AllWine.length} saved ${
+                userData.AllWine.length === 1 ? 'wine' : 'wines'
               }:`
-            : 'You have no saved books!'}
+            : 'You have no saved wines!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks?.map((book) => {
+          {userData.AllWine?.map((wine) => {
             return (
-              <Card key={book.bookId} border="dark">
-                {book.image ? (
+              <Card key={wine.wineId} border="dark">
+                {wine.image ? (
                   <Card.Img
-                    src={book.image}
-                    alt={`The cover for ${book.title}`}
+                    src={wine.image}
+                    alt={`The cover for ${wine.name}`}
                     variant="top"
                   />
                 ) : null}
                 <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <p className="small">Authors: {book.authors}</p>
-                  <Card.Text>{book.description}</Card.Text>
+                  <Card.Name>{wine.name}</Card.Name>
+                  <p className="small">Vineyard: {wine.vineyard}</p>
+                  <Card.Text>{wine.description}</Card.Text>
                   <Button
                     className="btn-block btn-danger"
-                    onClick={() => handleDeleteBook(book.bookId)}
+                    onClick={() => handleDeletewine(wine.wineId)}
                   >
-                    Delete this Book!
+                    Delete this wine!
                   </Button>
                 </Card.Body>
               </Card>
@@ -97,4 +73,4 @@ const SavedBooks = () => {
   );
 };
 
-export default SavedBooks;
+export default AllWine;
