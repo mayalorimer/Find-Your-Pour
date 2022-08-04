8354ba392aa16e1c17eeb061e2e6deb75cc1a101
aaloksikand <aalok.sikand@gmail.com>
2022-08-04 14:54:56 -0700

    fixed User.js and Signup.js

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,7 +1,7 @@
 import decode from 'jwt-decode';
 
-class AuthService {
-  getProfile() {
+class Auth {
+  getUser() {
     return decode(this.getToken());
   }
 
@@ -34,4 +34,4 @@ class AuthService {
   }
 }
 
-export default new AuthService();
+export default new Auth();
