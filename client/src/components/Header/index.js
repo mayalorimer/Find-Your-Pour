b5a80af389d453ace5856b08aa27d209b53d21a0
xndcmd <xndcmd@gmail.com>
2022-08-04 10:31:07 -0700

    added code to index.js

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,1 +1,13 @@
 // header / index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
