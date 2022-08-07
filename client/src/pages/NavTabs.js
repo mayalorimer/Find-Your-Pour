f6feb708455b2ee2a94a44143ca6dec680fe7728
aaloksikand <aalok.sikand@gmail.com>
2022-08-06 18:00:01 -0700

    importing react bootstrap to NavTabs.js to use Nav.Link

--- client/src/pages/NavTabs.js
+++ client/src/pages/NavTabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { Navbar, Nav, Container, Modal, Tab} from 'react-bootstrap';
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 import Auth from '../utils/auth';
